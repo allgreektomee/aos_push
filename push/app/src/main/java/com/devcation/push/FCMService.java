3f678e497723465e7279f78311cf02da27030ae4
devcation <88315076+devcation-learn@users.noreply.github.com>
2022-06-12 09:43:54 +0900

    fcmservice
    test

--- push/app/src/main/java/com/devcation/push/FCMService.java
+++ push/app/src/main/java/com/devcation/push/FCMService.java
@@ -82,4 +82,54 @@ public class FCMService extends FirebaseMessagingService {
 
     }
 
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+        Map<String, String> data = remoteMessage.getData();
+        String code1 = data.get("code");
+        Log.d(TAG, "code: " + code1);
+
+        String title1 = data.get("title");
+        Log.d(TAG, "title: " + title1);
+
+        String body1 = data.get("body");
+        Log.d(TAG, "body: " + body1);
+
+
+        //안드 화면 상단에 메세지 표시를 하기위한 알림
+        //상단 알림은 NotificationCompat.Builder, NotificationManagerCompat 이용해 생성하고 등록
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(getApplicationContext());
+
+        NotificationCompat.Builder builder = null;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) { //오레오 버전 이후 모든 알림은 채널을 할당
+
+            if (notificationManager.getNotificationChannel(CHANNEL_ID) == null) {
+                //채널은 고유한 ID, 채널이름 , 중요도
+                NotificationChannel channel = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT);
+                notificationManager.createNotificationChannel(channel); // 채널 등록
+//                notificationManager.createNotificationChannel(channel); 채널 삭제
+            }
+
+            builder = new NotificationCompat.Builder(getApplicationContext(), CHANNEL_ID); // 채널ID 등록
+        }else {
+            builder = new NotificationCompat.Builder(getApplicationContext());
+        }
+
+        String title = remoteMessage.getNotification().getTitle();
+        String body = remoteMessage.getNotification().getBody();
+
+        //알림 내용과 아이콘 설
+        builder.setContentTitle(title)
+                .setContentText(body)
+                .setSmallIcon(R.drawable.ic_launcher_foreground);
+
+        Notification notification = builder.build();
+        notificationManager.notify(1, notification);
+
+
+    }
+
 }
